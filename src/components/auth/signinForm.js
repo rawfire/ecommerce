9ec505d41f90737ea76145569bdfc39be34d88f3
Ignand <ignatius.and@gmail.com>
2020-01-12 16:59:37 -0500

    styled form field components

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -11,6 +11,7 @@ class SignInForm extends Component {
             <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
                 <Field className="sign-in-form__email" type="email" title="E-Mail" placeholder="e-mail" name="email" component={FormInput}/>
                 <Field className="sign-in-form__password" type="password" title="Password" placeholder="password" name="password" component={FormInput}/>
+                <div className="sign-in-form__line"></div>
                 <Field className="sign-in-form__login" onClick={() => console.log("submit")} type="submit" title="Login" name="login" component={FormButton}/>
             </form>
         )
