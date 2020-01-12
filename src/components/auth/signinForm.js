f0cccab8b7955b8841cb4acab5aab8229e1d09f3
Ignand <ignatius.and@gmail.com>
2020-01-12 14:24:36 -0500

    built signinform component for the signin component

---
+++ src/components/auth/signinForm.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+import { reduxForm, Filed } from "redux-form";
+
+class SignInForm extends Component {
+    render() {
+        const { className } = this.props;
+        return(
+            <form className={`${className} sign-in-form`}>
+                sign in form
+            </form>
+        )
+    }
+}
+SignInForm = reduxForm({
+    form: "SignInForm"
+})(SignInForm);
+
+export default SignInForm;
