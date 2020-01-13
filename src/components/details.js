e2230f7120cca95120f115f861e2bcd972ec0426
Ignand <ignatius.and@gmail.com>
2020-01-12 19:27:04 -0500

    styled details quick links component

--- src/components/details.js
+++ src/components/details.js
@@ -2,11 +2,11 @@ import React, {Component} from "react";
 
 class Details extends Component {
     render() {
-        const {title, links} = this.props;
+        const {className, links, title } = this.props;
         return(
-            <div className="details">
-                <div className="details-title">{title}</div>
-                <div className="details-links">
+            <div className={`${className} details`}>
+                <div className="details__title">{title}</div>
+                <div className="details__links">
                     {
                         links.map(link => {
                             return <a key={link._id} onClick={link.onClick} className="details__link">{link.title}</a>
