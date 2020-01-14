6764b2e0c65d5f8290ac37bec69d05f7be3509bd
Ignand <ignatius.and@gmail.com>
2020-01-14 16:41:58 -0500

    added password fields

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -7,6 +7,13 @@ import {FormInput, FormButton, LongGrayButton} from "../formFields";
 import history from "../../history";
 
 class AccountInformationForm extends Component {
+    constructor() {
+        super()
+
+        this.state ={
+            showPasswords: false
+        }
+    }
     render() {
         const { className, handleSubmit } = this.props;
         
@@ -22,7 +29,18 @@ class AccountInformationForm extends Component {
                 <Field className="account-information-form__zipcode" type="zipcode" title="Zipcode" placeholder="Zipcode" name="zipcode" component={FormInput}/>
 
 
-                <Field className="account-information-form__change-password" onClick={() => console.log("show buttons")} type="button" labelTitle="Password" title="Change Password" name="change-password" component={LongGrayButton}/>
+                {
+                    this.state.showPasswords ? 
+                    [
+                        <Field key={0} className="account-information-form__current" type="password" title="Current Password" placeholder="Current Password" name="current" component={FormInput}/>,
+                        <Field key={1} className="account-information-form__new" type="password" title="New Password" placeholder="New Password" name="new" component={FormInput}/>,
+                        <Field key={2} className="account-information-form__confirm" type="password" title="Confirm Password" placeholder="Confirm Password" name="confirm" component={FormInput}/>
+                    ]
+                    : 
+                    <Field className="account-information-form__change-password" onClick={() => this.setState({showPasswords: true})} type="button" labelTitle="Password" title="Change Password" name="change-password" component={LongGrayButton}/>
+
+                }
+
             </form>
         )
     }
