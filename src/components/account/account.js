fa46fa478b2899cb2a751f450280c76eb6b2998d
Ignand <ignatius.and@gmail.com>
2020-01-13 20:23:05 -0500

    setup purchase history component

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -26,13 +26,13 @@ class Account extends Component {
             {
                 _id: 0,
                 title: "Purchase History",
-                active: false,
+                active: true,
                 component: <PurchaseHistory/>
             },
             {
                 _id: 1,
                 title: "Account Information",
-                active: true,
+                active: false,
                 component: <AccountInformation/>
             }
         ]
@@ -54,7 +54,7 @@ class Account extends Component {
 
     render() {
         return(
-            <div>
+            <div className="account">
                 { this.renderContent() }
             </div>
         )
