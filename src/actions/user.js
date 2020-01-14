88140f5c6174fe9f49a1a710ba7c874f7f929a4e
Ignand <ignatius.and@gmail.com>
2020-01-13 22:21:17 -0500

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,13 +8,91 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: "purchase 1",
-                amount: 11.11
+                total: 2.23,
+                orderNumber: "A00B32CZ7",
+                orderDate: new Date(),
+                creditCard: "-0101",
+                user: {
+                    name: "John Smith",
+                    shippingAddress: "123 Main St"
+                }
             },
             {
                 _id: 1,
-                title: "purchase 2",
-                amount: 2.23
+                total: 12.23,
+                orderNumber: "A00B32CZ6",
+                orderDate: new Date(),
+                creditCard: "-0101",
+                user: {
+                    name: "Mike Sidis",
+                    shippingAddress: "123 Main St"
+                }
+            },
+            {
+                _id: 2,
+                total: 22.23,
+                orderNumber: "A00B32CZ5",
+                orderDate: new Date(),
+                creditCard: "-0101",
+                user: {
+                    name: "Paul McCartney",
+                    shippingAddress: "123 Main St"
+                }
+            },
+            {
+                _id: 3,
+                total: 1.23,
+                orderNumber: "A00B32CZ4",
+                orderDate: new Date(),
+                creditCard: "-0101",
+                user: {
+                    name: "John Lennon",
+                    shippingAddress: "123 Main St"
+                }
+            },
+            {
+                _id: 4,
+                total: 23.23,
+                orderNumber: "A00B32CZ3",
+                orderDate: new Date(),
+                creditCard: "-0101",
+                user: {
+                    name: "Ringo Star",
+                    shippingAddress: "123 Main St"
+                }
+            },
+            {
+                _id: 5,
+                total: 52.23,
+                orderNumber: "A00B32CZ2",
+                orderDate: new Date(),
+                creditCard: "-0101",
+                user: {
+                    name: "George Harrison",
+                    shippingAddress: "123 Main St"
+                }
+            },
+            {
+                _id: 6,
+                total: 23.23,
+                orderNumber: "A00B32CZ1",
+                orderDate: new Date(),
+                creditCard: "-0101",
+                user: {
+                    name: "Ignacio And",
+                    shippingAddress: "123 Main St"
+                }
+            },
+            {
+                _id: 7,
+                total: 2.23,
+                orderNumber: "A00B32CZZ",
+                orderDate: new Date(),
+                creditCard: "-0101",
+                user: {
+                    name: "Tiffany Smith",
+                    shippingAddress: "123 Main St"
+                }
             }
         ]
     })
