c3533b0b29b10b0d8d3bc3db6eec1e47e83ecbcf
Ignand <ignatius.and@gmail.com>
2020-01-17 16:06:21 -0500

    calculating order subtotal and total

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -8,8 +8,8 @@ import ReviewProducts from "./reviewProducts";
 
 class ReviewForm extends Component {
     render() {
-        const { className, handleSubmit } = this.props;
-        
+        const { className, handleSubmit, subtotal } = this.props;
+        let tax = 0.07
         return(
             <form onSubmit={handleSubmit} className={`${className} review-form`}>
                 <div className="review-form__legend">
@@ -24,19 +24,19 @@ class ReviewForm extends Component {
                 <div className="review-form__details review-details">
                     <div className="review-details__subtotal review-detail">
                         <div className="review-detail__title">Subtotal</div>
-                        <div className="review-detail__price">$3.99</div>
+                        <div className="review-detail__price">${subtotal}</div>
                     </div>
                     <div className="review-details__subtotal review-detail">
                         <div className="review-detail__title">Tax</div>
-                        <div className="review-detail__price">$2.99</div>
+                        <div className="review-detail__price">{tax}</div>
                     </div>
                     <div className="review-details__subtotal review-detail">
                         <div className="review-detail__title">SHipping</div>
-                        <div className="review-detail__price">$0.99</div>
+                        <div className="review-detail__price">$0.00</div>
                     </div>
                     <div className="review-details__total review-detail review-detail-green">
                         <div className="review-detail__title review-detail-green__title">Total</div>
-                        <div className="review-detail__price review-detail-green__price">$100.00</div>
+                        <div className="review-detail__price review-detail-green__price">${subtotal + tax}</div>
                     </div>
                 </div>
             </form>
