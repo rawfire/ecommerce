6f49eb6abcf26b031eb9408ad700c104ba11ab01
Ignand <ignatius.and@gmail.com>
2020-01-17 19:27:34 -0500

    finished shipping form component and sub components

--- src/components/information/orderSummary.js
+++ src/components/information/orderSummary.js
@@ -2,18 +2,33 @@ import React, {Component} from "react";
 
 import {UnderlineTitle, InfoTitle} from "./infoHelp";
 
+import {connect} from "react-redux"
 class OrderSummary extends Component {
     render() {
         const {className} = this.props;
+        let subtotal = 0;
+        let tax = 0.15;
+        let amountStickers = 0;
+        this.props.cartProducts.map(cartProduct => {
+            subtotal += cartProduct.quantity *cartProduct.product.price;
+            amountStickers += cartProduct.quantity;
+        })
         return(
             <div className={`${className} order-summary`}>
-                <UnderlineTitle className="order-summary__title"/>
-                <InfoTitle className="order-summary__subtotal" title="4 stickers" value="$7.96" />
-                <InfoTitle className="order-summary__tax-shipping" title="Taxes & Shipping" value="$2.96" />
-                <InfoTitle className="order-summary__total" title="Total" value="$9.02" />
+                <UnderlineTitle className="order-summary__title" title="Order Summary"/>
+                <InfoTitle className="order-summary__subtotal" title={`${amountStickers} stickers`} value={`$${subtotal}`} />
+                <InfoTitle className="order-summary__tax-shipping" title="Taxes & Shipping" value={`$${tax}`} />
+                <InfoTitle className="order-summary__total info-title-green" title="Total" value={`$${subtotal + tax}`} />
             </div>
         )
     }
 }
+
+function mapStateToprops(state){
+    const {cartProducts} = state.user;
+    return {cartProducts}
+}
+
+OrderSummary = connect(mapStateToprops)(OrderSummary);
 
 export default OrderSummary;
--- src/components/information/shippingForm.js
+++ src/components/information/shippingForm.js
@@ -25,7 +25,7 @@ class ShippingForm extends Component {
                 <div className="shipping-form__line"></div>
                 <Field className="shipping-form__back" onClick={() => history.push("/signin")} type="button" short={true} title="back" name="back" component={FormButton}/>
                 <Field className="shipping-form__use-this-address" onClick={() => history.push("/information/payment")} type="submit" title="Use This Address" name="use-this-address" component={FormButton}/>
-                <OrderSummary className="shipping-form__summary" />
+                <OrderSummary className="shipping-form__order-summary" />
             </form>
         )
     }
